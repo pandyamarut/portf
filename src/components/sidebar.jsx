ae5875c7ec8b0fe1b694db83bda2108d4efca47b
pandyamarut <pandyamarut@gmail.com>
2019-06-15 23:38:42 +0530

    minor updates

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -9,9 +9,9 @@ export default class Sidebar extends Component {
           <aside id="colorlib-aside" role="complementary" className="border js-fullheight">
             <div className="text-center">
               <div className="author-img" style={{backgroundImage: 'url(images/about.jpg)'}} />
-              <h1 id="colorlib-logo"><a href="index.html">Dhruv Barochiya</a></h1>
-              <span className="email"><i className="icon-mail"></i> Dhruv.barochiya34788@gmail.com</span>
-              <span className="number"><i className="icon-phone"></i> +91-886-600-5687</span>
+              <h1 id="colorlib-logo"><a href="index.html">Marut Pandya</a></h1>
+              <span className="email"><i className="icon-mail"></i>pandya.marut@gmail.com</span>
+              <span className="number"><i className="icon-phone"></i> +91-756-803-2998</span>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
@@ -26,12 +26,12 @@ export default class Sidebar extends Component {
             </nav>
             <nav id="colorlib-main-menu">
               <ul>
-                <li><a href="https://www.facebook.com/dhruv.barochia" target="_blank"><i className="icon-facebook2" /></a></li>
-                <li><a href="https://twitter.com/ddbarochiya" target="_blank"><i className="icon-twitter2" /></a></li>
+                <li><a href="https://www.facebook.com/marut.pandya" target="_blank"><i className="icon-facebook2" /></a></li>
+                <li><a href="https://twitter.com/pandya_marut" target="_blank"><i className="icon-twitter2" /></a></li>
                 <li><a href="https://www.instagram.com/ddbarochiya/" target="_blank"><i className="icon-instagram" /></a></li>
-                <li><a href="https://www.linkedin.com/in/dhruv-b-545b52a9/" target="_blank"><i className="icon-linkedin2" /></a></li>
-                <li><a href="https://github.com/Dhruv34788" target="_blank"><i className="icon-github"></i></a></li>
-                <li><a href="https://medium.com/@dhruv.barochia34788" target="_blank"><i className="icon-blogger2"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/marut-pandya-a16480131/" target="_blank"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/pandyamarut" target="_blank"><i className="icon-github"></i></a></li>
+                <li><a href="https://medium.com/me/stories/public" target="_blank"><i className="icon-blogger2"></i></a></li>
               </ul>
             </nav>
             <div className="colorlib-footer">
