26d579bf72cd3a880c6a1493c0a2823565c14bae
user <pandyamarut@gmail.com>
2020-04-08 11:40:07 +0530

    Updates

---
+++ precache-manifest.916e66277c4d7c716e687e5474d9b0ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af164c5cf7d8cf45bc28",
+    "url": "/portf/static/js/runtime~main.af164c5c.js"
+  },
+  {
+    "revision": "5793f1d0fc7eb2f3b4fb",
+    "url": "/portf/static/js/main.5793f1d0.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portf/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "5793f1d0fc7eb2f3b4fb",
+    "url": "/portf/static/css/main.c0e644b0.chunk.css"
+  },
+  {
+    "revision": "bd70b1d0c9de02116ccb22b4cef5520d",
+    "url": "/portf/index.html"
+  }
+];
